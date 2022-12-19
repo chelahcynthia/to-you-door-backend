030693af3d746a57b339d343e548b79245014f31
JeremyKungu <jeremikungu27@gmail.com>
2022-12-19 19:03:53 +0300

    added associations

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,3 +1,5 @@
 class CustomerSerializer < ActiveModel::Serializer
   attributes :id, :full_name, :email, :user_name, :password_digest
+
+  has_many :orders
 end
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,6 @@
 class OrderSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :phone_number, :delivery_address, :region, :city, :restaurant_id, :customer_id
+
+  belongs_to :restaurant
+  belongs_to :customer
 end
