9ab159b2a35544eedc885f5147fd7d214f593055
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-12-21 09:17:00 +0300

    P5P-22 CRUD operations created

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  resources :restaurant_meals
-  resources :meals
-  resources :orders
-  resources :restaurants
-  resources :customers
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurant_meals, only: [:create]
+  resources :meals, only: [:index, :show, :update, :create]
+  resources :orders, only: [:index, :create]
+  resources :restaurants, only: [:index, :show, :destroy]
+  resources :customers, only: [:show, :create, :update, :destroy]
 end
