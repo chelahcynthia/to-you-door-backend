6367d653d0abe2434249105f1002da3a81ceed93
Tressie Muthuri <tressiemuthuri@gmail.com>
2023-01-04 06:54:56 +0300

    P5P-27 resources created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
   resources :orders
   resources :restaurant_meals
   resources :meals
