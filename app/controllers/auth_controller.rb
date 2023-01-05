b42df5a132a112c798b12c74af5af8b0660f5937
Tressie Muthuri <tressiemuthuri@gmail.com>
2023-01-05 08:11:54 +0300

    P5P-27 authorization

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,18 +4,11 @@ class AuthController < ApplicationController
 
     def create
         customer = Customer.find_by(username: params[:username])
-        if customer && .authenticate(params[:password])
+        if customer && customer.authenticate(params[:password])
             token = encode_token({ cutomer_id: customer.id })
-            render json: { cutomer: CustomerSerializer.new(customer), jwt: token }, status: :accepted
+            render json: { customer: CustomerSerializer.new(customer), jwt: token }, status: :accepted
         else
             render json: { message: 'Invalid username or password' }, status: :unauthorized
         end
     end
-
-    # private
-
-    # def user_login_params
-    #     params.require(:user).permit(:username, :password)
-    # end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :users, only: [:create]
-  #     post '/login', to: 'auth#create'
-  #     get '/profile', to: 'users#profile'
-  #   end
-  # end
   resources :orders, only: [:index, :create]
   resources :restaurant_meals,only: [:index, :show, :destroy]
   resources :meals, only: [:index,:show, :update, :create]
