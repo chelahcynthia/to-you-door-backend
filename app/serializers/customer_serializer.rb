f0bdcb598db872822c6b2542eaa862eec436705a
dan muendo <danwalker254254@gmail.com>
2023-01-09 07:07:12 +0000

    updated validations

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,6 +1,6 @@
 class CustomerSerializer < ActiveModel::Serializer
 
-  attributes :id, :full_name, :email, :user_name, :password_digest
+  attributes :id, :first_name, :email, :username
 
   has_many :orders
 end
